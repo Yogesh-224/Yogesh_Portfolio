e7319fbdc3e0808814c1f9c6b99332eabbfe480c
Yogesh-224 <yogeshpandey224@gmail.com>
2024-12-11 00:02:48 +0530

    Porfolio website basic

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu ul");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
