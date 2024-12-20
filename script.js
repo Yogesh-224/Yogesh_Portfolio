94fb78456bf57ee1cf85a3cc1e119adae1e425e1
Yogesh-224 <yogeshpandey224@gmail.com>
2024-12-20 23:28:25 +0530

    basic formatting

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const navMenu = document.querySelector(".nav-menu ul");
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+// Open the modal
+function openContactModal() {
+  document.getElementById("contact-modal").style.display = "block";
+}
+
+// Close the modal
+function closeContactModal() {
+  document.getElementById("contact-modal").style.display = "none";
+}
+
+// Close the modal if clicked outside the modal content
+window.onclick = function (event) {
+  var modal = document.getElementById("contact-modal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
